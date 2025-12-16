94a763a106daab36e82b0fc39f19b0efa71e620a
john.mclaren <john.mclaren@hornetdefence.com>
2025-12-16 13:57:18 +1300

    Tweaking

--- Jam25/Entities/Enemies/Enemy.cs
+++ Jam25/Entities/Enemies/Enemy.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HDT.Gaming.Models;
 using HDT.Gaming.Physics;
 using Jam25.Entities.Enemies.Controllers;
 using Jam25.Graphics;
-using Jam25.Models;
 
 namespace Jam25.Entities.Enemies
 {
